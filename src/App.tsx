f4222ff6c93767d757360a6d9af6af65b9ebede1
ToshihiroWatanabe <79039863+ToshihiroWatanabe@users.noreply.github.com>
2021-07-23 12:47:27 +0900

    update: ポモドーロタイマー完成

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,99 @@
-import React from "react";
+import React, { useState } from "react";
 
-function App() {
-  return <></>;
+/** 一度にカウントする秒数 */
+const ONCE_COUNT = 1;
+/** カウントの間隔(ミリ秒) */
+const COUNT_INTERVAL = 1000;
+/** カウントのsetIntervalのID */
+let countInterval = 0;
+
+/** タイマーモード */
+type TimerMode = "work" | "break";
+
+/** タイマーの長さ */
+const TIMER_LENGTH = { work: 25 * 60, break: 5 * 60 } as const;
+type TIMER_LENGTH = typeof TIMER_LENGTH[keyof typeof TIMER_LENGTH];
+
+interface State {
+  timeLeft: number;
+  timerMode: TimerMode;
+  isTimerOn: boolean;
 }
+
+/**
+ * 秒の数値をMM:SS形式の文字列に変換します。
+ * @param {number} second 秒
+ * @returns MM:SS形式の文字列
+ */
+const secondToMMSS = (second: number) => {
+  const MM =
+    second >= 10 * 60
+      ? Math.floor(second / 60).toString()
+      : second >= 1 * 60
+      ? "0" + Math.floor(second / 60).toString()
+      : "00";
+  const SS = second % 60 >= 10 ? second % 60 : "0" + (second % 60);
+  return MM + ":" + SS;
+};
+
+const App = () => {
+  const [state, setState] = useState<State>({
+    timeLeft: TIMER_LENGTH.work,
+    timerMode: "work",
+    isTimerOn: false,
+  });
+
+  const onClick = () => {
+    setState((state) => {
+      clearInterval(countInterval);
+      if (state.isTimerOn) {
+        return {
+          ...state,
+          timeLeft: TIMER_LENGTH.work,
+          timerMode: "work",
+          isTimerOn: false,
+        };
+      } else {
+        countInterval = window.setInterval(() => {
+          count();
+        }, COUNT_INTERVAL);
+        return { ...state, isTimerOn: true };
+      }
+    });
+  };
+
+  const count = () => {
+    setState((state) => {
+      if (state.timeLeft < 1) {
+        state = toggleTimerMode(state);
+      }
+      return { ...state, timeLeft: state.timeLeft - ONCE_COUNT };
+    });
+  };
+
+  const toggleTimerMode = (state: State): State => {
+    if (state.timerMode === "work") {
+      return {
+        ...state,
+        timeLeft: TIMER_LENGTH.break,
+        timerMode: "break",
+      };
+    } else {
+      return {
+        ...state,
+        timeLeft: TIMER_LENGTH.work,
+        timerMode: "work",
+      };
+    }
+  };
+
+  return (
+    <>
+      <div>{secondToMMSS(state.timeLeft)}</div>
+      <button onClick={onClick}>{state.isTimerOn ? "停止" : "開始"}</button>
+      <div>{state.timerMode === "work" ? "作業" : "休憩"}</div>
+    </>
+  );
+};
 
 export default App;
