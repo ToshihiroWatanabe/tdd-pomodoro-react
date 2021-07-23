fffe80113aa74a548981d478f1cac5bdfc029d8e
ToshihiroWatanabe <79039863+ToshihiroWatanabe@users.noreply.github.com>
2021-07-23 23:01:26 +0900

    red: 「25:00」が描画されていること

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,7 +3,10 @@ import { render, screen } from "@testing-library/react";
 import App from "./App";
 
 describe("初期表示", () => {
-  test.todo("「25:00」が描画されていること");
+  test("「25:00」が描画されていること", () => {
+    const { getByTestId } = render(<App />);
+    expect(getByTestId("timeLeft").textContent).toEqual("25:00");
+  });
   test.todo("「開始」が描画されていること");
   test.todo("「作業」が描画されていること");
 });
