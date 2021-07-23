4b1edbe9cfa7060f545d932618d3b45065522036
ToshihiroWatanabe <79039863+ToshihiroWatanabe@users.noreply.github.com>
2021-07-23 22:42:14 +0900

    update: 初期表示のテストのtodoを追加

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("初期表示", () => {
+  test.todo("「25:00」が描画されていること");
+  test.todo("「開始」が描画されていること");
+  test.todo("「作業」が描画されていること");
 });
