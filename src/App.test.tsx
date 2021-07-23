162baddcb52917497c924aba9ea81f4d01ad825e
ToshihiroWatanabe <79039863+ToshihiroWatanabe@users.noreply.github.com>
2021-07-23 23:19:51 +0900

    red: 「開始」が描画されていること

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import App from "./App";
 
 describe("初期表示", () => {
@@ -7,6 +7,9 @@ describe("初期表示", () => {
     const { getByTestId } = render(<App />);
     expect(getByTestId("timeLeft").textContent).toEqual("25:00");
   });
-  test.todo("「開始」が描画されていること");
+  test("「開始」が描画されていること", () => {
+    const { getByTestId } = render(<App />);
+    expect(getByTestId("timerButton").textContent).toEqual("開始");
+  });
   test.todo("「作業」が描画されていること");
 });
