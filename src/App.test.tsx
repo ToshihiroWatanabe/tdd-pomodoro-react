7af485ed1233f7a1024d7ba0b3776f9b24e76333
ToshihiroWatanabe <79039863+ToshihiroWatanabe@users.noreply.github.com>
2021-07-24 19:07:26 +0900

    red: 作業と休憩が切り替わるタイミングの表示のテスト

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -90,11 +90,43 @@ describe("開始ボタンを押した後の表示のテスト", () => {
     });
   });
   describe("開始ボタンを押してから25分+1秒後の表示のテスト", () => {
-    test.todo("「04:59」が描画されていること");
-    test.todo("「休憩」が描画されていること");
+    test("「04:59」が描画されていること", async () => {
+      jest.useFakeTimers();
+      const { getByTestId } = render(<App />);
+      userEvent.click(getByTestId("timerButton"));
+      act(() => {
+        jest.advanceTimersByTime((25 * 60 + 1) * 1000);
+      });
+      expect(getByTestId("timeLeft").textContent).toEqual("04:59");
+    });
+    test("「休憩」が描画されていること", async () => {
+      jest.useFakeTimers();
+      const { getByTestId } = render(<App />);
+      userEvent.click(getByTestId("timerButton"));
+      act(() => {
+        jest.advanceTimersByTime((25 * 60 + 1) * 1000);
+      });
+      expect(getByTestId("timerMode").textContent).toEqual("休憩");
+    });
   });
   describe("開始ボタンを押してから25分+5分+1秒後の表示のテスト", () => {
-    test.todo("「24:59」が描画されていること");
-    test.todo("「作業」が描画されていること");
+    test("「24:59」が描画されていること", async () => {
+      jest.useFakeTimers();
+      const { getByTestId } = render(<App />);
+      userEvent.click(getByTestId("timerButton"));
+      act(() => {
+        jest.advanceTimersByTime((25 * 60 + 5 * 60 + 1) * 1000);
+      });
+      expect(getByTestId("timeLeft").textContent).toEqual("24:59");
+    });
+    test("「作業」が描画されていること", async () => {
+      jest.useFakeTimers();
+      const { getByTestId } = render(<App />);
+      userEvent.click(getByTestId("timerButton"));
+      act(() => {
+        jest.advanceTimersByTime((25 * 60 + 5 * 60 + 1) * 1000);
+      });
+      expect(getByTestId("timerMode").textContent).toEqual("作業");
+    });
   });
 });
